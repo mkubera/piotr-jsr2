21173d5bb936761560b5f654bd02c8a979629184
mkubera <rainteller@tutanota.de>
2022-02-16 23:04:15 +0100

    Added Switch. URL fixed

--- src/components/CurencyApp.jsx
+++ src/components/CurencyApp.jsx
@@ -7,8 +7,9 @@ const CurrencyApp = () => {
   const [chosenAmount, setChosenAmount] = useState(initChosenAmount);
   const [chosenCurrency, setChosenCurrency] = useState(initChosenCurrency);
   const [currencies, setCurrencies] = useState(initCurrencies);
+  const [isLeft, setIsLeft] = useState(true);
   const NBP_API_URL =
-    "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+    "https://api.nbp.pl/api/exchangerates/tables/a/?format=json";
 
   useEffect(() => {
     fetch(NBP_API_URL)
@@ -31,38 +32,53 @@ const CurrencyApp = () => {
   const changeChosenAmount = (e) => setChosenAmount(e.target.value);
   const changeChosenCurrency = (e) => setChosenCurrency(e.target.value);
 
+  const ValuesLeft = () => {
+    return (
+      <div className="values-left">
+        <input
+          type="number"
+          onChange={changeChosenAmount}
+          value={chosenAmount}
+        />
+        <select onChange={changeChosenCurrency} value={chosenCurrency}>
+          {currencies.map(({ code }) => (
+            <option key={code} value={code}>
+              {code}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  };
+
+  const ValuesRight = () => {
+    return (
+      <div className="values-right">
+        <input
+          type="number"
+          value={calculatedCurrency()}
+          className="amount2"
+          disabled
+        />
+        <select id="currency2">
+          <option value="PLN">PLN</option>
+        </select>
+      </div>
+    );
+  };
+
   return (
     <div className="container">
       <h1>PRZELICZNIK KURSÓW WALUT </h1>
       <p>Sprawdź dzisiejsze kursy </p>
+
       <div className="values">
-        <div className="values-left">
-          <input
-            type="number"
-            onChange={changeChosenAmount}
-            value={chosenAmount}
-          />
-          <select onChange={changeChosenCurrency} value={chosenCurrency}>
-            {currencies.map(({ code }) => (
-              <option key={code} value={code}>
-                {code}
-              </option>
-            ))}
-          </select>
-        </div>
+        {isLeft ? <ValuesLeft /> : <ValuesRight />}
         <p className="equals">=</p>
-        <div className="values-right">
-          <input
-            type="number"
-            value={calculatedCurrency()}
-            className="amount2"
-            disabled
-          />
-          <select id="currency2">
-            <option value="PLN">PLN</option>
-          </select>
-        </div>
+        {isLeft ? <ValuesRight /> : <ValuesLeft />}
       </div>
+
+      <button onClick={(e) => setIsLeft((state) => !state)}>Switch</button>
     </div>
   );
 };
